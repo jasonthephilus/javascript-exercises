4c3a0558040d2a9247713875a9592145e8fa06fc
Jason Theophilus <jasontheophilussurjadi@gmail.com>
2022-04-25 21:39:55 +0700

    finished 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(text, n) {
+    const arr = [];
+    if (n >= 0) {
+        for(i = 1; i <= n; i++) {
+            arr[i-1] = text;
+        }
+        
+        return(arr.join(""));
+    }
+    else {
+        return("ERROR");
+    }
 };
 
 // Do not edit below this line
