785bfd56dde77f65bf3490c75c3510d4ca056a42
Jason Theophilus <jasontheophilussurjadi@gmail.com>
2022-04-27 18:22:27 +0700

    finished 04_removeFromArray

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    const arr = input.split("");
+    let arrrev = [];
+    for (i=0; i<=arr.length; i++) {
+        arrrev[i] = arr[(arr.length - i)];
+    }
 
+    return arrrev.join("");
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
 const removeFromArray = function() {
-
+    arr = arguments[0];
+    for (n = 1; n <= (arguments.length -1); n++){
+        for (i=0; i<=arr.length; i++) {
+            if(arr[i] === arguments[n]) {
+                arr.splice(i,1);
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
