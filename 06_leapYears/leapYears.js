23bf04f7ea55096390adb3d767095f7daf610dda
Jason Theophilus <jasontheophilussurjadi@gmail.com>
2022-05-01 13:21:43 +0700

    finished 05_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    divBy4 = year%4;
+    divBy100 = year%100;
+    divBy400 = year%400;
+    console.log(divBy4, divBy400)
+    if (divBy400 === 0) {
+        return true;
+    }
+    else if (divBy100 !==0 && divBy4===0) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
