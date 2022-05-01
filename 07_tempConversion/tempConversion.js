7b59badfc81af48ed1d16fbaf48342317b62d78e
Jason Theophilus <jasontheophilussurjadi@gmail.com>
2022-05-01 13:29:42 +0700

    finished 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(input) {
+  output = (input-32)*5/9;
+  output = Math.round(output*10) / 10;
+  return output; 
 };
 
-const ctof = function() {
-
+const ctof = function(input) {
+  output = (input*9/5)+32;
+  output = Math.round(output*10)/10;
+  return output;
 };
 
 // Do not edit below this line
