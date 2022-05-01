c04e6b58a135205573ffdd5099ab1227cd994b1c
Jason Theophilus <jasontheophilussurjadi@gmail.com>
2022-05-01 14:13:32 +0700

    finished 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    text = text.replace(/[^a-z]/gi, "");
+    text = text.toLowerCase();
+    arr = text.split("");
+    len = arr.length; console.log(len);
+    for (i = 0; i < len; i++) {
+        console.log(arr[i], arr[(len-i-1)]);
+        if (arr[i] == arr[len-i-1]) {
+            result = true;
+        }
+        else {
+            result = false;
+            break
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
