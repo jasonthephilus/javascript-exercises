d958630d53b4b6be7383c6e35abb64e6630fc7f3
Jason Theophilus <jasontheophilussurjadi@gmail.com>
2022-05-01 14:29:42 +0700

    finished 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = parseInt(n);
+    if (n <= 0) {
+        return "OOPS";
+    }
+    else {
+        n0 = 0;
+        n1 = 1;
+        total = 1;
+        for (i = 1; i < n; i++) {
+            total = n0 + n1;
+            n0 = n1;
+            n1 = total;
+        }
+        return total;
+    }
 };
 
 // Do not edit below this line
