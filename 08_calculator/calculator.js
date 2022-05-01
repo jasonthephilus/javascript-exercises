1b2098d7f04f4336536d5c5f067cd1cd1937ccf5
Jason Theophilus <jasontheophilussurjadi@gmail.com>
2022-05-01 13:55:31 +0700

    finished 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,54 @@
 const add = function() {
-	
+  return arguments[0]+arguments[1];
 };
 
 const subtract = function() {
-	
+	return arguments[0]-arguments[1];
 };
 
 const sum = function() {
-	
+  let total = 0;
+  arr = arguments[0];
+  if (arr.length !== 0) {
+    for (i = 0; i < arr.length; i++) {
+      total = total + parseFloat(arr[i]);
+    }
+  }
+  else {
+    total = 0;
+  }
+  return total;
 };
 
 const multiply = function() {
-
+  let total = 1;
+  arr = arguments[0];
+  if (arr.length !== 0) {
+    for (i = 0; i < arr.length; i++) {
+      total = total * parseFloat(arr[i]);
+    }
+  }
+  else {
+    total = 0;
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(n, p) {
+  return Math.pow(n, p);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let total = 1;
+  if (arr.length !== 0) {
+    for (i = n; i > 0; i--) {
+      total = total * i;
+    }
+  }
+  else {
+    total = 1;
+  }
+  return total;
 };
 
 // Do not edit below this line
