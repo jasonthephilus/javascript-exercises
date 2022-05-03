f376d12862b07d84cea6703fa181b50cbdd0fb52
Jason Theophilus <jasontheophilussurjadi@gmail.com>
2022-05-03 14:35:05 +0700

    finished 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,28 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(people) {
+    today = new Date();
+    n = people.length;
+    for (i = 0; i < n; i++) {
+        if (people[i].yearOfDeath === undefined) {
+            people[i].age = today.getFullYear() - people[i].yearOfBirth;
+        }
+        else{
+            people[i].age = people[i].yearOfDeath - people[i].yearOfBirth;
+        }
+        console.log(people[i])
+    }
+    oldest = people[0];
+    for (i = 0; i < (n-1); i++) {
+        if (oldest.age > people[i+1].age) {
+            oldest = oldest;
+        }
+        else {
+            oldest = people[i+1];
+        }
+    }
+    console.log(oldest.age, oldest.name);
+    return oldest;
+}
+    
 
 // Do not edit below this line
 module.exports = findTheOldest;
