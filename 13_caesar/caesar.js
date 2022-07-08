8dd8c833c7746590103d23184528e84f45947b91
Jason Theophilus <jasontheophilussurjadi@gmail.com>
2022-07-08 19:08:57 +0700

    finished caesar

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,51 @@
-const caesar = function() {
+const caesar = function (text, caesarvalue) {
+    caesarvalue = caesarvalue % 26;
+    textstr = Array.from(text);
+    textuni = textstr.map(function (e) {
+        return e.charCodeAt(0);
+    })
 
+    textcaesar = textuni.map(function (e) {
+        //check if lower/upper
+        value = e + caesarvalue;
+        if (e >= 65 && e <= 90) {
+            //upper
+            if (value > 90) {
+                return value - 26;
+            }
+            else if (value < 65) {
+                return value + 26;
+            }
+            else {
+                return value;
+            }
+        }
+        else if (e >= 97 && e <= 122) {
+            //lower
+            if (value > 122) {
+                return value - 26;
+            }
+            else if (value < 97) {
+                return value + 26;
+            }
+            else {
+                return value;
+            }
+        }
+        else {
+            //nonlatin
+            return e;
+        }
+    })
+
+    textconverted = textcaesar.map(function (e) {
+
+        return String.fromCharCode(e)
+    }).join("")
+    console.log(textuni)
+    return textconverted;
 };
+
 
 // Do not edit below this line
 module.exports = caesar;
